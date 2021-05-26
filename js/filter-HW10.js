b24bc06510dc37c2ecf87a66ebaaa8af4a932f9f
AndreyFaichuk <76702228+AndreyFaichuk@users.noreply.github.com>
2021-05-26 18:29:24 +0300

    HW10 update

--- js/filter-HW10.js
+++ js/filter-HW10.js
@@ -18,66 +18,204 @@ for (let i = 0; i < newObjOfItems.length; i++) {
     if(newObjOfItems[i].display === null) newObjOfItems[i].display = 'none'
 }
 
+
+const slider = item => {
+    const slider = document.createElement('div')
+    slider.className = 'slider'
+
+
+    const sliderButton = document.createElement('button')
+    sliderButton.className = 'button_top_ipad'
+    sliderButton.innerText = 'Read more info'
+
+    slider.innerHTML = `
+
+        <div class="ipad">
+                   <img src="${item.imgUrl}" alt="pic">
+                   <div class="buttonSliderWrapper">
+                    <h1>${item.name}</h1>
+                    <h3>${item.price} $</h3>
+                    </div>
+                </div>
+        `
+
+    let sliderButtons = slider.querySelector('.ipad .buttonSliderWrapper')
+    sliderButtons.append(sliderButton)
+
+    sliderButton.id = item.id
+
+    sliderButton.onclick = function (){
+        showModal(item)
+    }
+
+
+    return slider
+
+}
+const renderSlide = newObjOfItems => {
+    const containerElem = document.querySelector('.sliderWrapper')
+    newObjOfItems.forEach(item => {
+        const card = slider(item)
+        containerElem.append(card)
+    })
+}
+
+renderSlide(newObjOfItems)
+
+
+const showModal = data => {
+    const modal = document.querySelector('.modal')
+    modal.onclick = (event) => {
+        if (modal === event.target) {
+            document.body.style.overflow = ''
+            return modal.classList.remove('modal_active')
+        }
+    }
+
+    modal.classList.add('modal_active')
+    document.body.style.overflow = 'hidden'
+
+    const buttonInModal = document.createElement('button')    /*добавляем кнопку в модальное окно*/
+    buttonInModal.className = 'modalButton'
+    buttonInModal.innerText = 'Add to cart'
+
+    if (data.orderInfo.inStock === 0) buttonInModal.setAttribute('class', 'modalButton_disabled')
+
+
+    modal.querySelector('.modal_container').innerHTML = `
+
+        <div class="modal_image">
+            <img src="${data.imgUrl}" alt="pic">
+        </div>
+
+         <div class="modal_info">
+            <h1>${data.name}</h1>
+            <div class="card_1_bottom">
+                <div class="avarageWrapper">
+                    <p class="views"> <span class="rating">${data.orderInfo.reviews}%</span> Positive reviews</p>
+                    <span class="aboveAvarage">Above avarage</span>
+                </div>
+                <div class="amountOfOrdersWrapper">
+                    <span class="amountOfOrders">${data.orderInfo.orders}</span>
+                    <p class="orders"> orders</p>
+                </div>
+
+            </div>
+            <div class="infoAboutItem">
+                <ul>
+                    <li><p>Color: <span>${data.color}</span></p></li>
+                    <li><p>Operating System: <span>${data.os}</span> </p></li>
+                    <li><p>Chip: <span>${data.chip.name}</span></p></li>
+                    <li><p>Cores: <span>${data.chip.cores}</span></p></li>
+                    <li><p>Height: <span>${data.size.height} cm</span></p></li>
+                    <li><p>Width: <span>${data.size.width} cm</span></p></li>
+                    <li><p>Depth: <span>${data.size.depth} cm</span></p></li>
+                    <li><p>Weight: <span>${data.size.weight} g</span></p></li>
+                </ul>
+            </div>
+        </div>
+        <div class="modal_priceAndButton">
+                <h1>$ ${data.price}</h1>
+                <p>Stock: <span class="modal_Pcs">${data.orderInfo.inStock}</span> pcs.</p>
+        </div>
+    `
+
+    let modalButton = document.querySelector('.modal .modal_priceAndButton')
+    modalButton.append(buttonInModal)
+
+    modalButton.id = data.id
+
+    modalButton.onclick = (event) => {
+        if (modalButton !== event.target && event.target.className === 'modalButton') {
+            console.log(modalButton.id)
+        }
+    }
+
+    return modal
+}
+
+
+
 class Cards {
     constructor() {
         this.renderCards(newObjOfItems)
         this.bindInputChange()
+
     }
 
     renderCards(arr){
         let container = document.querySelector('.wrapper')
         container.innerHTML = ''
 
+            if(!arr.length){
+                container.innerHTML = `<h1>Items not found</h1>`
+            }
 
         arr.forEach(item => {
-            const image = document.createElement('img')
-            image.setAttribute('src', `${item.imgUrl}`)  /*добавляем изображение*/
-            image.className = '.image_wrap img'
 
-            container.innerHTML += `
+            const card = document.createElement('div')
+            card.className = "card_1"
 
-                  <div class="card_1">
+            const imageHeart = document.createElement('img')
+            imageHeart.setAttribute('src', "img/icons/like_empty.svg")   /*добавляем верхнюю кнопку*/
+            imageHeart.className = '.card_1 button[type=submit]'
 
+            card.innerHTML = `
                 <button type="submit"></button>
 
                  <div class="image_wrap">
 
-
+                <img src="${item.imgUrl}" class="image_wrap img">
             <h1>${item.name}</h1>
 
              <div class="checkk">
                 <img src="${item.stockPage}" alt="pic">
-            <p class="check"> <span class="amount">${item.orderInfo.inStock}</span> left in stock</p>
-            </div>
-            <div class="butt_wrapper">
-             <p class="price">Price: <span class="price">${item.price}</span> $</p>
+                <p class="check"> <span class="amount">${item.orderInfo.inStock}</span> left in stock</p>
+                </div>
+                <div class="butt_wrapper">
+                 <p class="price">Price: <span class="price">${item.price}</span> $</p>
              </div>
 
           </div>
 
-         <div class="card_1_bottom">
+                <div class="card_1_bottom">
             <div class="avarageWrapper">
             <p class="views"> <span class="rating">${item.orderInfo.reviews}%</span> Positive reviews</p>
                 <span class="aboveAvarage">Above avarage</span>
-            </div>
-                <div class="amountOfOrdersWrapper">
-                <span class="amountOfOrders">${item.orderInfo.orders}</span>
-                    <p class="orders"> orders</p>
                 </div>
+                    <div class="amountOfOrdersWrapper">
+                    <span class="amountOfOrders">${item.orderInfo.orders}</span>
+                        <p class="orders"> orders</p>
+                    </div>
 
             </div>
-        </div>
             `
 
+            const button = document.createElement('button')    /*добавляем кнопку*/
+            button.className = 'button_card_1'
+            button.innerText = 'Read more info'
+
+
+            if (item.orderInfo.inStock === 0) button.setAttribute('class', 'button_card_1_disabled')
+
+            let wrap = card.querySelector('.butt_wrapper')
+            wrap.append(button)
+            container.append(card)
+
+            let buttonType = card.querySelector('button[type=submit]')
+                buttonType.prepend(imageHeart)
+
+            buttonType.addEventListener('click', function () {
+                        item.isFavorite = !item.isFavorite
+                imageHeart.setAttribute('src', `img/icons/${item.isFavorite ? 'heart.svg' : 'like_empty.svg'}`)
+            })
+
+            button.onclick = function () {
+                showModal(item)
+                goTopBtn.className = 'back_to_top'
+            }
         })
 
-        // const button = document.createElement('button')    /*добавляем кнопку*/
-        // button.className = 'button_card_1'
-        // button.innerText = 'Read more info'
-        //
-        //
-        // let wrap = container.querySelector('.butt_wrapper')
-        // wrap.append(button)
 
     }
 
@@ -87,6 +225,7 @@ class Cards {
             findService.search = e.target.value
         }
     }
+
 
 }
 
@@ -100,11 +239,11 @@ class FindService{
     #os = []
     #colors = []
     #memory = []
-    #display = []
     constructor() {
-        this.search = ''
 
     }
+
+
 
     get search (){
         return this.#search
@@ -123,10 +262,10 @@ class FindService{
         if(this.#colors.includes(color)){
             this.#colors = this.#colors.filter(c => color !== c)
             return
-        } else {
+        }
             this.#colors.push(color)
             this.renderFilteredItems()
-        }
+
 
     }
 
@@ -141,7 +280,6 @@ class FindService{
         }
             this.#os.push(os)
             this.renderFilteredItems()
-
     }
 
     get memory (){
@@ -150,12 +288,13 @@ class FindService{
 
     set memory (memory){
         if(this.#memory.includes(memory)){
+            debugger
             this.#memory = this.#memory.filter(c => memory !== c)
             return
-        } else {
+        }
             this.#memory.push(memory)
             this.renderFilteredItems()
-        }
+
 
     }
 
@@ -178,6 +317,7 @@ class FindService{
             let isOsEx = !this.os.length
 
             this.os.forEach(os => {
+                debugger
                 if(item.os === os){
                     isOsEx = true
                 }
@@ -190,16 +330,15 @@ class FindService{
 
             let isMemoryEx = !this.memory.length
 
-            this.memory.forEach(storage => {
-                if(item.storage === storage){
+            this.memory.forEach(memory => {
+                if(item.storage === memory){
+                    debugger
                     isMemoryEx = true
                 }
             })
             if(!isMemoryEx){
                 return false
             }
-
-
         })
     }
 
